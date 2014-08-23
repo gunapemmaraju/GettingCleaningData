a3f8f16117b431c025f599535d5736c2d0892f46
Gunaranjan Pemmaraju <gunaranjan.pemmaraju@gmail.com>
2014-08-23 20:51:48 +0530

    Code for the Project

---
+++ run_analysis.R
@@ -0,0 +1,52 @@
+#Step1: I am merging the Xs and Ys respectively, But I will not be colBinding X and Y in this step.
+# I will keep them separate for now, and will ColBind them as needed.
+
+X_test<-read.table("./UCI HAR Dataset/test/X_test.txt")
+X_train<-read.table("./UCI HAR Dataset/train/X_train.txt")
+X1<-rbind(X_test,X_train)
+
+Y_test<-read.table("./UCI HAR Dataset/test/Y_test.txt")
+Y_train<-read.table("./UCI HAR Dataset/train/Y_train.txt")
+Y1<-rbind(Y_test,Y_train)
+
+sub_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+sub_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
+sub1<-rbind(sub_test,sub_train)
+colnames(sub1)<-"Subject"
+    
+#Step2 - Get only those variables which are COMPUTED means and Standard deviations. I am assuming that any variable with ()
+# are the ones that are computed.
+feat<-read.table("UCI HAR Dataset//features.txt")
+reqdVars<-(grepl("mean()",feat[,2],fixed = TRUE) | grepl("std()",feat[,2],fixed = TRUE))
+sum(reqdVars)
+colInds<-which(reqdVars)
+X2<-X1[,colInds]
+#str(X2)
+
+#Step3: I am still keeping the X's and Y's separately. I wan't sure if this merging was required
+actLabels<-read.table("./UCI HAR Dataset//activity_labels.txt")
+Y3<-merge(Y1,actLabels,by.x=1,by.y=1,all.x = TRUE)
+Y3[,1]<-NULL
+colnames(Y3)<-"ActivityName"
+
+#Step4
+X4<-X2
+colnames(X4)<-feat[colInds,2]
+
+#Step5. Now I will bind X and Y, as well as the subject.
+df<-cbind(sub1,Y3,X4)
+
+#Step5 - First the Value columns are extracted. And then using split apply combine, the required tidying is achieved. 
+# The finally the column names are appropriately set. And the the tidy Data set is created.
+vals<-df[,3:ncol(df)]
+groups<-split(vals,paste(df$Subject,df$ActivityName, sep="+"))
+res1<-sapply(groups,colMeans)
+row.names(res1)<-paste("Average",row.names(res1),sep="")
+res2<-data.frame(t(res1))
+subAct<-rownames(res2)
+Subject <- as.integer(sapply(strsplit(subAct, "+", fixed=T), "[[", 1))
+Activity <- as.character(sapply(strsplit(subAct, "+",fixed=T), "[[", 2))
+result<-cbind(Subject,Activity,res2)
+write.table(result,file = "tidy.txt",row.names=FALSE, quote = FALSE)
+#DONE
+
